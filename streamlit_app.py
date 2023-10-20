fe0723cb0c2cd11524ef00e82a395fb33c0019ad
SamyKNB <148531489+SamyKNB@users.noreply.github.com>
2023-10-20 10:14:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
-import streamlit as st
+import streamlit
 
  # Titre
-    st.title("My Parents New Healthy Diner")
+ streamlit.title("My Parents New Healthy Diner")
     
     # Sous-titre
-    st.subheader("Breakfast Menu")
+ streamlit.subheader("Breakfast Menu")
     
     # Textes
-    st.text("Omega 3 & Blueberry Oatmeal")
-    st.text("Kale, Spinach & Rocket Smoothie")
-    st.text("Hard-Boiled Free Range Egg")
+ streamlit.text("Omega 3 & Blueberry Oatmeal")
+ streamlit.text("Kale, Spinach & Rocket Smoothie")
+ streamlit.text("Hard-Boiled Free Range Egg")
 
