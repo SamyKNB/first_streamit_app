3837bff06f40e0e756796f0ef1cf6744c39fc4bb
SamyKNB <148531489+SamyKNB@users.noreply.github.com>
2023-10-20 10:15:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,10 @@
 import streamlit
 
- # Titre
+
  streamlit.title("My Parents New Healthy Diner")
-    
-    # Sous-titre
+
  streamlit.subheader("Breakfast Menu")
     
-    # Textes
  streamlit.text("Omega 3 & Blueberry Oatmeal")
  streamlit.text("Kale, Spinach & Rocket Smoothie")
  streamlit.text("Hard-Boiled Free Range Egg")
